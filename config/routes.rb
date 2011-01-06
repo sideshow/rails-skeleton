385144075394df1d99d44ea921004d06950b585f
sideshow <adam@digirati.co.nz>
2011-01-06 22:19:31 +1300

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RailsSkeleton::Application.routes.draw do
+  get "home/index"
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +53,9 @@ RailsSkeleton::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  
+  root :to => "home#index"
+  
 
   # See how all your routes lay out with "rake routes"
 
